619ff51152d54d61058fd05b81cf3dd4e4aa051e
SyedNazmusSakib-SNS <syednazmussakib64@gamil.com>
2025-04-27 00:50:21 +0600

    just to check

---
+++ lab1.py
@@ -0,0 +1,20 @@
+variables = ["WA", "NT", "SA", "Q", "NSW", "V", "T"]
+
+constraints = {
+        "WA" : ["NT", "SA"],
+        "NT" : ["WA", "SA", "Q"],
+        "SA" : ["WA", "NT", "Q", "NSW", "V"],
+        "Q"  : ["NT", "SA", "NSW"],
+        "NSW" : ["SA", "Q", "V"],
+        "V"   : ["SA", "NSW"],
+        "T" : []
+
+        }
+
+
+domains = {}
+
+for var in variables:
+        domains[var] = ["red", "green", "blue"]
+print(domains)
+
